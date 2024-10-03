106f5b3201821478744d9a3b74fb955d17bfd680
bok000111 <bok000111@naver.com>
2024-10-04 00:26:37 +0900

    move react directory

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Loader2 } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { greet } from '&/wasm';
 
 const App: React.FC = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 text-gray-800">
       <div className="text-center">
         <h1 className="text-5xl font-bold">JBOK</h1>
-        <p className="text-lg mt-2">This site is currently under development.</p>
+        <Button onClick={greet} className="mt-4">
+          This button will trigger a WASM function
+        </Button>
         <div className="flex justify-center mt-8">
           <Loader2 className="w-12 h-12 animate-spin text-blue-500" />
         </div>
--- frontend/react/vite.config.ts
+++ frontend/react/vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import wasm from 'vite-plugin-wasm'
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    wasm(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "&": path.resolve(__dirname, "../wasm/pkg"),
     },
+  },
+  build: {
+    target: "es2022",
   },
 })
